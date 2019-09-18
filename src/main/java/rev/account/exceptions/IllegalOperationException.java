0d9c52e5967085901233f67b82020de8dc7c0c6f
Varun Tak <varun.tak@sap.com>
2019-09-18 12:35:31 +0800

    added an exception class for command failure

---
+++ src/main/java/rev/account/exceptions/IllegalOperationException.java
@@ -0,0 +1,10 @@
+package rev.account.exceptions;
+
+/**
+ * Created by i316946 on 16/9/19.
+ */
+public class IllegalOperationException extends Throwable {
+    public IllegalOperationException(String s) {
+        super(s);
+    }
+}
