67bbe3efe14ae4891e5ce5f790ee04d3cb5a0c9f
Varun Tak <varun.tak@sap.com>
2019-09-20 12:44:31 +0800

    removed list based command execution

--- src/main/java/rev/account/command/TransferMoneyCommand.java
+++ src/main/java/rev/account/command/TransferMoneyCommand.java
@@ -3,27 +3,17 @@ package rev.account.command;
 import com.google.inject.Inject;
 import rev.account.exceptions.CommandFailureException;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by i316946 on 18/9/19.
  */
 public class TransferMoneyCommand implements AccountCommand {
-    private List<AccountCommand> commands = new ArrayList<>();
     private WithdrawalCommand withdrawalCommand;
     private DepositCommand depositCommand;
-
-    public TransferMoneyCommand(List<AccountCommand> commandList) {
-        this.commands = commandList;
-    }
 
     @Inject
     public TransferMoneyCommand(WithdrawalCommand withdrawalCommand, DepositCommand depositCommand){
         this.withdrawalCommand = withdrawalCommand;
         this.depositCommand = depositCommand;
-        this.commands.add(this.withdrawalCommand);
-        this.commands.add(this.depositCommand);
     }
 
     @Override
@@ -41,10 +31,8 @@ public class TransferMoneyCommand implements AccountCommand {
 //    TODO: to throw unrecoverable exception
     @Override
     public void rollback() {
-        for (AccountCommand command: commands
-                ) {
-            command.rollback();
-        }
+        withdrawalCommand.rollback();
+        depositCommand.rollback();
     }
 
     public WithdrawalCommand getWithdrawalCommand() {
